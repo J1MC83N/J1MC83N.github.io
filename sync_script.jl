8646ab8319aae330639e9ec285606e4ce3a0a33c
J1MC83N <sevenred.chen@outlook.com>
2025-06-29 20:00:54 +0800

    updated main page

--- sync_script.jl
+++ sync_script.jl
@@ -4,6 +4,6 @@ optimize(prepath="",minify=false)
 `rsync -r __site/ ../J1MC83N.github.io` |> run
 cd("../J1MC83N.github.io")
 `git add --all` |> run
-`git commit -m "added dithering article"` |> run
+`git commit -m "updated main page"` |> run
 `git push` |> run
 cd("../main")
