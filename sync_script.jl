36226f096816c7c77bca36c43ac6015f3c253645
J1MC83N <sevenred.chen@outlook.com>
2025-07-09 23:48:43 +0900

    reduced preview sizes

--- sync_script.jl
+++ sync_script.jl
@@ -4,6 +4,6 @@ optimize(prepath="",minify=false)
 `rsync -r __site/ ../J1MC83N.github.io` |> run
 cd("../J1MC83N.github.io")
 `git add --all` |> run
-`git commit -m "update profile picture crop"` |> run
+`git commit -m "reduced preview sizes"` |> run
 `git push` |> run
 cd("../main")
