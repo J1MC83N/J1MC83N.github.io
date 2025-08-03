db2c55bef8508f6c4edc1ed8b89e79ad5444591f
J1MC83N <sevenred.chen@outlook.com>
2025-08-03 01:35:32 -0700

    fixed wrong uploads

--- sync_script.jl
+++ sync_script.jl
@@ -1,10 +1,9 @@
 using Franklin
-serve()
+serve(launch=true)
 optimize(prepath="",minify=false)
-verify_links()
-`rsync -r __site/ ../J1MC83N.github.io` |> run
+`rsync -r --delete __site/ ../J1MC83N.github.io` |> run
 cd("../J1MC83N.github.io")
 `git add --all` |> run
-`git commit -m "updated main page"` |> run
+`git commit -m "removed sidebar from article pages; added obfusticated email; added automatic preview image compression to reduce subdir page size; updated content"` |> run
 `git push` |> run
 cd("../main")
