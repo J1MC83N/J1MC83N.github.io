06e061e6ab1704c7b3d2a89db0d92929895b1b5e
J1MC83N <sevenred.chen@outlook.com>
2025-09-18 00:42:37 -0700

    added galleries

--- sync_script.jl
+++ sync_script.jl
@@ -1,9 +1,9 @@
 using Franklin
 serve(launch=true)
 optimize(prepath="",minify=false)
-`rsync -r --delete __site/ ../J1MC83N.github.io` |> run
+`rsync -av --delete --exclude=".git/" __site/ ../J1MC83N.github.io/` |> run
 cd("../J1MC83N.github.io")
 `git add --all` |> run
-`git commit -m "removed sidebar from article pages; added obfusticated email; added automatic preview image compression to reduce subdir page size; updated content"` |> run
+`git commit -m "fixed wrong uploads"` |> run
 `git push` |> run
-cd("../main")
+cd("../main-minimal-mistakes")
